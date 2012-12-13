428b255b7c2e5155d8afdd5182e37a61be2e84f6
leandromarino <leandromarino@leandromarino.com.br>
2012-12-13 14:59:32 -0800

    Commit TbAmo.r

--- TbAmo.r
+++ TbAmo.r
@@ -1,19 +1,24 @@
 ################################################################################
 #####======================================================================#####
 #####                                                                      #####
-#####   IDENTIFICAÇÃO                                                      #####
+#####   IDENTIFICACAO                                                      #####
 #####                                                                      #####
 #####======================================================================#####
 ################################################################################
 
-### DEFINIÇÃO DO LOCAL DE TRABALHO E VARIÁVEIS DE AMBIENTE
-setwd('I:\\Mestrado ENCE 2012\\03 - Amostragem\\TrabalhoFinal\\')
+###   DEFINICAO DO TRABALHO E DE VARIAVEIS DE AMBIENTE
 options(width=180)
 
-### CARREGANDO OS PACOTES NECESSÁRIOS
+
+#instalar pacote a partir de zip local
+#install.packages('C:\\Users\\leandromarino\\Documents\\Projetos\\TBAmoMunic\\trunk\\RCurl_1.95-3.zip')
+
+### CARREGANDO OS PACOTES NECESSARIOS
 library(xlsReadWrite)
 library(SOAR)
 library(survey)
+require(RCurl)
+require(foreign)
 
 
 
@@ -21,31 +26,39 @@ library(survey)
 ### LEITURA DOS DADOS
 ## Inicialmente o arquivo estava em formato .xlsx e foi convertido no Excel 2010
 ## para o .xls
-classcol = c(rep('character',5),rep('numeric',2),rep('character',2))
 url <- 'https://raw.github.com/leandromarino/TBAmoMunic/master/Munic.dat'
-munic <- read.table(url,colClasses=classcol,sep='\t',header=T)
-rm(classcol)
+url <- getURL(url,ssl.verifypeer = FALSE)
+munic <- read.table(textConnection(url), colClasses='character',header=T,quote='',
+         sep='\t')
 str(munic)
 
 
+
 #####----------------------------------------------------------------------#####
 #####----------------------------------------------------------------------#####
-### ITEM 1 - Use os valores populacionais das variáveis de interesse para
-### determinar o CV esperado para os estimadores dos parâmetros de interesse.
+### ITEM 1 - Use os valores populacionais das variaveis de interesse para
+### determinar o CV esperado para os estimadores dos parametros de interesse.
 
-#Y1 = FuncADMD    - Total de funcionários ativos da administração direta;
-#Y2 = Maternidade - Existe Maternidade no município?
-#Y3 = Emergencia  - Existe Unidade de emergência no município?
+#Y1 = FuncADMD    - Total de funcionario ativos da administracao direta;
+#Y2 = Maternidade - Existe Maternidade no municipio?
+#Y3 = Emergencia  - Existe Unidade de emergencia no municipio?
 munic <- transform(munic,
-            func =FuncADMD,
-            mater=ifelse(Maternidade=="Sim",1,ifelse(Maternidade=='Não',0,NA)),
-            emerg=ifelse(Emergencia=="Sim",1,ifelse(Emergencia=='Não',0,NA)))
+            Populacao=as.integer(Populacao),
+            FuncADMD=as.integer(FuncADMD),
+            Regiao=substr(CodMunic,1,1))
 munic <- transform(munic,
-            materemerg = ifelse(rowSums(cbind(mater,emerg)) == 2,1,0),
-            )
-
+            e1_func  = FuncADMD,
+            e2_raz   = Populacao/FuncADMD,
+            e3_mater = ifelse(Maternidade=="Sim",1,ifelse(Maternidade=='N�o',0,NA)),
+            emerg    = ifelse(Emergencia=="Sim",1,ifelse(Emergencia=='N�o',0,NA)))
+munic <- transform(munic,
+            e4_materemerg = ifelse(rowSums(cbind(e3_mater,emerg)) == 2,1,0))
 munic[1:10,]
-varint <- c('func','mater','emerg')
+
+
+
+
+varint <- c('e1_func','e2_raz','e3_mater','e4_materemerg')
 (mdPop <- apply(munic[,varint],2,mean,na.rm=T))
 (sdPop <- apply(munic[,varint],2,sd,na.rm=T))
 (cvPop <- sdPop/mdPop)
